19103839018523cfb22899c2155f3db0369ab475
Tushar Tiwari <122177680+tushar45264@users.noreply.github.com>
2025-01-09 03:00:12 +0530

    Update jwt.js

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -6,7 +6,7 @@ export const generateToken = (id,res) => {
     const cookieOptions = {
         expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
         // httpOnly: true, 
-        // sameSite: "None",
+        sameSite: "None",
         secure: true,  
     };
     res.cookie("token", token, cookieOptions);
