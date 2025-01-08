553519988377383f1800d5a602af6cc1baf5ae1b
Tushar Tiwari <122177680+tushar45264@users.noreply.github.com>
2025-01-09 02:34:38 +0530

    Update server.js

--- backend/socket/server.js
+++ backend/socket/server.js
@@ -1,10 +1,10 @@
-import http from 'http';
+import https from 'https';
 import { Server } from 'socket.io';
 import express from 'express';
 import Chat from '../models/Chat.js';
 
 const app = express();
-const server = http.createServer(app);
+const server = https.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
